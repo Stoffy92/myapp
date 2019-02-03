fe63fbe00264582065b26a07c169c03490cc876c
Stefan <stoffberg.stefan@gmail.com>
2019-02-03 17:35:36 +1300

    Updates

---
+++ precache-manifest.5a764da8ccc7fa77491291671da94900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "4cf2e03d036caf941521",
+    "url": "/myapp/static/js/main.4cf2e03d.chunk.js"
+  },
+  {
+    "revision": "43c0385e5572bd089f9c",
+    "url": "/myapp/static/js/1.43c0385e.chunk.js"
+  },
+  {
+    "revision": "4cf2e03d036caf941521",
+    "url": "/myapp/static/css/main.ef01f6f0.chunk.css"
+  },
+  {
+    "revision": "43c0385e5572bd089f9c",
+    "url": "/myapp/static/css/1.bd52c1f3.chunk.css"
+  },
+  {
+    "revision": "f2d5c69904206d7267615165c94e2c8d",
+    "url": "/myapp/index.html"
+  }
+];
