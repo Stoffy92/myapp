c347e2fc09b514a6585ba6bed853e35337ca7059
Stefan <stoffberg.stefan@gmail.com>
2019-02-04 16:36:30 +1300

    fixing routing

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 // import { Router } from 'react-router';
 import createHashHistory from 'history/createHashHistory';
+import { BrowserRouter, browserHistory } from 'react-router-dom';
 
-import { HashRouter } from 'react-router-dom';
 
 const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 
 ReactDOM.render(
-  <HashRouter history={hashHistory}>
+  <BrowserRouter history={browserHistory}>
     <App />
-  </HashRouter>, 
+  </BrowserRouter>, 
   document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
